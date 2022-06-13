8a969ece18f80722afcbfb98dbd836092cfbf119
evdrug <druzyakin.e@gmail.com>
2022-06-13 20:49:16 +0300

    decomposition __main__ (#16)
    * decomposition __main__
    
    * refactoring code
    
    Co-authored-by: evgesha <e.druzyakin@yandex.ru>

--- storage_test/mongo/generate_data.py
+++ storage_test/mongo/generate_data.py
@@ -1,6 +1,7 @@
 import logging
 import random
 from datetime import datetime
+from functools import partial
 from uuid import uuid4
 
 import lorem
@@ -26,7 +27,7 @@ collections_settings: dict = {
 }
 
 
-def generate_bookmarks(user_id):
+def generate_bookmarks(movie_ids, user_id):
     return {
         'user_id': user_id,
         'movies': random.sample(movie_ids, k=random.randint(10, MAX_BOOKMARKS))
@@ -37,7 +38,7 @@ def generate_likes(user_ids):
     return [uid for uid in random.sample(user_ids, k=random.randint(10, MAX_LIKES))]
 
 
-def generate_movie_likes(movie_id):
+def generate_movie_likes(user_ids, movie_id):
     like_by = generate_likes(user_ids)
     dislike_by = generate_likes(user_ids)
     rating = len(like_by) * 10 / (len(like_by) + len(dislike_by))
@@ -49,7 +50,7 @@ def generate_movie_likes(movie_id):
     }
 
 
-def generate_review(movie_id):
+def generate_review(user_ids, movie_id):
     user_id = random.choice(user_ids)
     like_by = generate_likes(user_ids)
     dislike_by = generate_likes(user_ids)
@@ -82,44 +83,44 @@ def insert_data(collection, collection_ids, func, count_reviews):
         collection.insert_many(data, ordered=False)
 
 
-if __name__ == '__main__':
+def init_collections_data():
     user_ids = [str(uuid4()) for _ in range(COUNT_USERS)]
     movie_ids = [str(uuid4()) for _ in range(COUNT_MOVIES)]
-    collections_data = {
+    return {
         'review': {
             'collection_ids': movie_ids,
-            'func': generate_review,
+            'func': partial(generate_review, user_ids),
             'count_reviews': COUNT_REVIEWS,
             'indexes': ['rating', 'like_by', 'dislike_by']
         },
         'user_bookmarks': {
             'collection_ids': user_ids,
-            'func': generate_bookmarks,
+            'func': partial(generate_bookmarks, movie_ids),
             'count_reviews': len(user_ids),
             'indexes': ['bookmarks']
         },
         'movie_likes': {
             'collection_ids': movie_ids,
-            'func': generate_movie_likes,
+            'func': partial(generate_movie_likes, user_ids),
             'count_reviews': COUNT_REVIEWS,
             'indexes': ['rating', 'like_by', 'dislike_by']
         }
     }
 
-    client = MongoClient('localhost', 27017)
-    db = client[DB_NAME]
 
+def create_collections(client_db, settings):
     logger.info("Start setting collections")
-    for collection_name, config in collections_settings.items():
-        db[collection_name]
+    for collection_name, config in settings.items():
         shard_key = config.get('shard_key', None)
         if shard_key:
-            client.admin.command('enableSharding', DB_NAME)
-            client.admin.command('shardCollection', '{}.{}'.format(DB_NAME, collection_name),
-                                 key={shard_key: "hashed"})
+            client_db.admin.command('enableSharding', DB_NAME)
+            client_db.admin.command('shardCollection', '{}.{}'.format(DB_NAME, collection_name),
+                                    key={shard_key: "hashed"})
 
+
+def generate_data(db, structure_data):
     logger.info("Started creating data")
-    for collection_name, setting in collections_data.items():
+    for collection_name, setting in structure_data.items():
         logger.info("Started insert data to collection: {}".format(collection_name))
         collection = db[collection_name]
         insert_data(
@@ -133,3 +134,11 @@ if __name__ == '__main__':
             for index in indexes:
                 logger.info("Started create index '{}' to collection: {}".format(index, collection_name))
                 collection.create_index(index)
+
+
+if __name__ == '__main__':
+    collections_data = init_collections_data()
+    client = MongoClient('localhost', 27017)
+    database = client[DB_NAME]
+    create_collections(client, collections_settings)
+    generate_data(database, collections_data)
